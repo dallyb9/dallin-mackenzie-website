bc93ff24c604ec6be8ba5a554e7ea89309802a9a
Dallin Bentley <dallyb9@gmail.com>
2024-06-14 12:45:44 -0600

    second commit, edited css and JavaScript

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript code for interactive features can be added here
+document.getElementById("contact-form").addEventListener("submit", function(event){
+    event.preventDefault();
+    alert("Thank you for your message!");
+});
